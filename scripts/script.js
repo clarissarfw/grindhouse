33b31948185077cdcc3660054b58c32ba414cbb6
Clarissa <clarissafw@gmail.com>
2019-11-20 14:50:27 -0800

    yay

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
+<img class="swapme" src="../SVG/sclogo.svg">  
+<script>
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <script>
+  	$('.swapme').mouseover(function(){
+  		$('.swapme').attr('src', 'http://placekitten.com/400/300')
+  	})
+  	$('.swapme').mouseout(function(){
+  		$('.swapme').attr('src', 'http://placekitten.com/400/600')
+  	})
+  </script>
 
 
 
